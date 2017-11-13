563045879ccad1330d14de0af4127ea0b67bd873
SolomonHD <shilli2@localhost.localdomain>
2017-11-13 07:57:15 -0800

    change development deploy to mysql-change

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,6 +1,6 @@
 role :app, %w{deploy@whpihdev1}
 role :web, %w{deploy@whpihdev1}
 role :db, %w{deploy@whpihdev1}, :primary => true
-set :branch, "develop"
+set :branch, "mysql-change"
 set :rails_env, "development"
 set :deploy_to, "/home/deploy/Apps/online-investigators-guide/"
