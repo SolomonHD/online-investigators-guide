f4aaa6950061265ac992815de705a0197a89dfea
SolomonHD <shilli2@localhost.localdomain>
2017-11-14 10:30:20 -0800

    Add dev enviroment check to omniauth, change certs on box

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,16 +9,18 @@
 #    ]
 #  }
 #end
-Rails.application.config.middleware.use OmniAuth::Builder do
+if Rails.env.development?
+ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :saml,
     :assertion_consumer_service_url     => "https://oig-dev.emory.edu/auth/saml/callback",
     :issuer                             => "https://oig-dev.emory.edu",
     :idp_sso_target_url                 => "https://login.emory.edu/idp/profile/SAML2/Redirect/SSO",
-   # :idp_sso_target_url_runtime_params  => {:original_request_param => :mapped_idp_param},
+   # :idp_sso_target_url_runtime_params => {:original_request_param => :mapped_idp_param},
     :idp_cert                           => ENV['SHIBB_IDP_CERT'],
-   # :idp_cert_fingerprint               => "E7:91:B2:E1:...",
-   # :idp_cert_fingerprint_validator     => lambda { |fingerprint| fingerprint },
-   # :name_identifier_format             => "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
-     :certificate                      => ENV['OIG_DEV_SP_CERT'],
-     :private_key                      => ENV['OIG_DEV_SP_KEY']
+   # :idp_cert_fingerprint              => "E7:91:B2:E1:...",
+   # :idp_cert_fingerprint_validator    => lambda { |fingerprint| fingerprint },
+   # :name_identifier_format            => "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+     :certificate                       => ENV['OIG_DEV_SP_CERT'],
+     :private_key                       => ENV['OIG_DEV_SP_KEY']
+ end
 end
