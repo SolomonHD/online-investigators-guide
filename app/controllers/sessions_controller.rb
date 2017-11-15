805d227ac8a29e3d825e6842340a1c3d640cc707
SolomonHD <shilli2@localhost.localdomain>
2017-11-15 12:54:29 -0800

    Add basic omniauth logic to routes and sessionctl

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-
+=begin 
   def create
     user = User.find_by(net_id: params[:session][:net_id])
     if user
@@ -12,10 +12,28 @@ class SessionsController < ApplicationController
       flash.now.notice = "Net ID is invalid"
       render "new"
     end
-  end
+  end 
+
 
   def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
+=end
+
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
+
+
 end
