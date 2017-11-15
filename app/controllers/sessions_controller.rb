c22118db31b4a82bbe09c56bc9880ba905dd329a
SolomonHD <shilli2@localhost.localdomain>
2017-11-15 12:56:54 -0800

    syntax fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,6 @@ class SessionsController < ApplicationController
   end
 =end
 
-class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     self.current_user = @user
