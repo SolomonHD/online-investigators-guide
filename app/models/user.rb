0d2c9e844398646df05d5c65bee021c814024487
Solomon Hill <shilli2@localhost.localdomain>
2017-11-15 18:31:20 -0800

    Change user model and routes to create session after auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
   has_many :surveys
 
-  def name
-    first_name + " " + last_name
-  end
+def self.from_omniauth(auth)
+   # Rails.logger.debug "auth = #{auth.inspect}"
+    # Uncomment the debugger above to capture what a shib auth object looks like for testing
+    user = where(provider: auth.provider, uid: auth.info.uid).first_or_create
+    # user.display_name = auth.info.display_name
+    user.uid = auth.info.uid
+    #user.ppid = auth.uid
+    #user.email = auth.info.uid + '@emory.edu'
+    user.save
+    user
+ end 
+
+ def name
+   first_name + " " + last_name
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,14 @@ Rails.application.routes.draw do
   resources :pages, :except => ['edit', 'new', 'update', 'destroy']
   resources :users do
     resources :surveys
+    get 'sign_in', to: 'omniauth#new', as: :new_user_session
+    post 'sign_in', to: 'omniauth_callbacks#saml', as: :new_session
+    #get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
 
   root 'pages#index'
-  get '/auth/:provider/callback', to: 'sessions#create'
-  post '/auth/:provider/callback', to: 'sessions#create'
+  
+  #get '/auth/:provider/callback', to: 'sessions#create'
+  #post '/auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
