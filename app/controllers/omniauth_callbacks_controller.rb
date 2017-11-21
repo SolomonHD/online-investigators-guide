071ad124d26cf9f724875dc27c78e3c7c613c2f2
tbparrish <terry.parrish@emory.edu>
2017-11-21 10:49:18 -0500

    Test

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class OmniauthCallbacksController < ApplicationController
       @user = User.from_omniauth(request.env["omniauth.auth"])
       # set_flash_message :notice, :success, kind: "Emory NetID"
       # sign_in_and_redirect @user
-      logger.info '===============> ' + @user.name + " " + @user.id.to_s " " + @user.id.to_s
+      logger.info '===============> ' + @user.name + " " + @user.id.to_s + " " + @user.is_admin.to_s
       redirect_to '/'
    end
   end
