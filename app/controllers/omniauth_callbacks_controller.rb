7929793eeb543f34ba319b2f3c41f0d462059bb6
tbparrish <terry.parrish@emory.edu>
2017-11-21 14:01:09 -0500

    Test

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class OmniauthCallbacksController < ApplicationController
       @user = User.from_omniauth(request.env["omniauth.auth"])
       # set_flash_message :notice, :success, kind: "Emory NetID"
       # sign_in_and_redirect @user\
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       logger.info '===============> ' + @user.name + " " + @user.id.to_s + " " + @user.is_admin.to_s
       if @user.is_admin
         redirect_to '/admin', notice: "Logged in!"
