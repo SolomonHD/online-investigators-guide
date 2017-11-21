1e444ca8094a79ed89827e72054de830c643fe3f
tbparrish <terry.parrish@emory.edu>
2017-11-21 09:49:33 -0500

    Test

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,12 @@
 #class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 class OmniauthCallbacksController < ApplicationController
     def saml
-      Rails.logger.debug "OmniauthCallbacksController#saml: request.env['omniauth.auth']: #{request.env['omniauth.auth']}"
+      # Rails.logger.debug "OmniauthCallbacksController#saml: request.env['omniauth.auth']: #{request.env['omniauth.auth']}"
       # had to create the `from_omniauth(auth_hash)` class method on our User model
       @user = User.from_omniauth(request.env["omniauth.auth"])
       # set_flash_message :notice, :success, kind: "Emory NetID"
       # sign_in_and_redirect @user
-      logger.info '#{user.inspect}'
+      logger.info #{user.inspect}
       logger.info '===============> ' + @user.name + " " + @user.id.to_s + " " + @user.is_admin.to_s
       redirect_to 'user_surveys_url(@user)'
    end
