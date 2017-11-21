2af5a13aaee1660c1b3652b4abd78808e1b8abaa
Solomon Hill <shilli2@localhost.localdomain>
2017-11-20 20:58:20 -0800

    bugfix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -32,7 +32,7 @@ if Rails.env.development?
      :certificate                       => ENV['OIG_DEV_SP_CERT'],
      :private_key                       => ENV['OIG_DEV_SP_KEY'],
     # :info_fields => { :uid => 'uid'},
-     :attribute_statements => {:last_name => 'urn:oid:2.5.4.4', :first_name => 'urn:oid:2.5.4.42'},
+     :attribute_statements => {:last_name => ['urn:oid:2.5.4.4'], :first_name => ['urn:oid:2.5.4.42']},
      :uid_attribute => 'urn:oid:0.9.2342.19200300.100.1.1',
      :security => {
        #:authn_requests_signed             => true, # goes on md SPSSODescriptor tag
