a56a8b97bc84511d09b8c0dd5f64216635286fcc
Saundra Barrett <sbarre5@emory.edu>
2017-11-27 15:18:52 -0500

    Added nullset and removed login

--- app/models/admin/broken_link.rb
+++ app/models/admin/broken_link.rb
@@ -2,6 +2,7 @@ class Admin::BrokenLink < ApplicationRecord
 
   def self.runReport
 
+    # Remove all broken links in database
     Admin::BrokenLink.delete_all
 
     require 'mechanize'
@@ -11,18 +12,23 @@ class Admin::BrokenLink < ApplicationRecord
     agent.user_agent_alias = 'Windows Chrome'
     page = agent.get('https://oig-dev.emory.edu/sitemap')
 
-    page.links
-
+    # GET PAGES FROM SITEMAP
     page.links_with(href: %r{.*/pages/\w+}).each do |link|
+      # FETCH URL USING MECHANIZE AGENT
       internalPage = agent.get(link.href)
+        # GET ALL LINKS ON URL
         internalPage.links.each do |l|
           begin
            l.click.code.to_s
+          # PREVENT ERRORS AND WRITE TO DATABASE
           rescue Mechanize::ResponseCodeError => e
             case e.response_code
               when "404"
                 puts "caught Net :: HTTPNotFound!"
+                # REMOVE LOGIN INFORMATION FROM BROKEN LINK REPORT
+                if !l.href.include?("auth/")
                  Admin::BrokenLink.create(page_id: link.uri.to_s.split('/')[-1], link_text: l.text, page_title: link.text, broken_url: l.uri, link_status: e.response_code)
+                end
                 next # If the page can not be found next
               when "502"
                 puts "caught Net :: HTTPBadGateway!"
@@ -32,99 +38,11 @@ class Admin::BrokenLink < ApplicationRecord
                 retry
               end
           end
-
         end
     end
-
-
-    # page = agent.get('https://oig-dev.emory.edu/sitemap')
-    # page.links.internal.each do |l|
-    #   puts l.split('/')[-1]
-    #   begin
-    #     page = agent.get(l)
-    #     puts l
-    #     page.links.each do |crawl|
-    #       puts crawl.text
-    #       puts crawl.href
-    #       # puts crawl.click.code.to_s
-    #     end
-    #       # puts page.links
-    #       # puts page.title
-    #   rescue Mechanize::ResponseCodeError => e
-    #       case e.response_code
-    #       when "404"
-    #         puts page.link.text
-    #         puts page.link.uri
-    #         puts "caught Net :: HTTPNotFound!"
-    #         # Admin::BrokenLink.create(page_id: l.split('/')[-1], link_text: page.link.text, page_title: page.link.text, broken_url: page.link.uri, link_status: e.response_code)
-    #         next # If the page can not be found next
-    #       when "502"
-    #         puts "caught Net :: HTTPBadGateway!"
-    #         retry # another time when you can not access it well!
-    #       else
-    #         puts "caught Excepcion!" + e.response_code
-    #         retry
-    #       end
-    #     puts page
-    #   end
-
-      # if !l.include?("#")
-      #   #crawl internal link
-      #   @internalLink = MetaInspector.new(l, faraday_options: { ssl: { verify: false } })
-      #   # puts @internalLink
-      #   #get all external links
-      #   @internalLink.links.external.each do |el|
-      #       @newPage = agent.get(el)
-      #       page = begin
-      #         puts @newPage
-      #       rescue Mechanize::ResponseCodeError => e
-      #         puts e.response_code
-      #       end
-          #skip PDF and PPTX
-            # if !el.include?(".pdf") && !el.include?(".pptx")
-            #   #crawl external link
-            #
-            #   @newPage = agent.get(el)
-            #   puts @newPage.link.text
-            #   @externalLink = MetaInspector.new(el, faraday_options: { ssl: { verify: false } })
-            #   # puts @externalLink.text
-            #   # if response code is anything other than success
-            #   if @externalLink.response.status != 200
-            #     # puts @externalLink
-            #     # create new broken link in database
-            #     # Admin::BrokenLink.create(page_id: @internalLink.url.split('/')[-1], page_title: @internalLink.best_title, url: @externalLink.url, link_status: @externalLink.response.status)
-            #   end
-            # end
-          # end
-      # end
-      #
-      # puts link
-      #   puts link.response
-        # newPage = agent.get(link)
-        # begin
-        #   puts newPage
-        # rescue Mechanize::ResponseCodeError => e
-        #   puts e.response_code
-        # end
-        # puts newPage
-        # begin
-        #   # puts link.uri.match(/\pages/)
-        #   if link.uri =~ '\pages'
-        #     puts link.text
-        #     # puts link.uri.include?('pages')
-        #   end
-        #   # puts link.text
-        #   #
-        #   # newPage = agent.get(link.uri)
-        #   # puts newPage.links
-        #   if link.click.code != '200'
-        #     # Create Broken Link in Database
-        #   end
-        # rescue Mechanize::ResponseCodeError => e
-        #   # puts e.response_code # the status code as a string, e.g. "404"
-        # end
-
-      # end
-
+    # IF NO DEADLINKS, RUN NULLSET FOR UPDATED TIME
+    if Admin::BrokenLink.count == 0
+      Admin::BrokenLink.create(page_id:null)
+    end
   end
 end
