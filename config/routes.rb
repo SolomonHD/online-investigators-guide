6eb2755c6593b0f7cf143699ede07fdae08e5bef
tbparrish <terry.parrish@emory.edu>
2017-11-29 10:45:07 -0500

    Handle shibb logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  post 'logout', to: 'omniauth_callbacks#logout'
+
+
   namespace :admin do
     resources :pages do
         get :move
