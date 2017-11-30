4601d6c9ec75d7535754e5c9cd531042180513d3
tbparrish <terry.parrish@emory.edu>
2017-11-29 19:38:28 -0500

    Corrected deploying branch to develop

--- config/routes.rb
+++ config/routes.rb
@@ -29,17 +29,10 @@ Rails.application.routes.draw do
   resources :pages, :except => ['edit', 'new', 'update', 'destroy']
   resources :users do
     resources :surveys
-   # get 'sign_in', to: 'omniauth#new', as: :new_user_session
-   # post 'sign_in', to: 'omniauth_callbacks#saml', as: :new_session
-    #get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
   get 'sitemap', to:'pages#sitemap', as: 'sitemap'
   match  '/auth/:provider/callback', :to => 'omniauth_callbacks#saml', via: [:get, :post]
   match  'pages/auth/:provider/callback', :to => 'omniauth_callbacks#saml', via: [:get, :post]
   root 'pages#index'
   get '/search' => 'pages#search'
-
-  #get '/auth/:provider/callback', to: 'sessions#create'
-  #post '/auth/:provider/callback', to: 'sessions#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
