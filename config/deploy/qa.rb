c157a706de09952fab3074a5ae89c292b93ac9bf
Solomon Hill <shilli2@localhost.localdomain>
2017-11-30 21:11:33 -0800

    Change enviroment qa to qa

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -2,5 +2,5 @@ role :app, %w{deploy@whrailsqa1}
 role :web, %w{deploy@whrailsqa1}
 role :db, %w{deploy@whrailsqa1}, :primary => true
 set :branch, "feature/create-user-table"
-set :rails_env, "production"
+set :rails_env, "qa"
 set :deploy_to, "/home/deploy/Apps/online-investigators-guide/"
