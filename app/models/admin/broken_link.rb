04df9a1de939ddd8c98955b8565bafb18aafc516
Saundra Barrett <sbarre5@emory.edu>
2017-12-21 12:53:35 -0500

    Debug link checker

--- app/models/admin/broken_link.rb
+++ app/models/admin/broken_link.rb
@@ -31,6 +31,22 @@ class Admin::BrokenLink < ApplicationRecord
           begin
            l.click.code.to_s
           # PREVENT ERRORS AND WRITE TO DATABASE
+        rescue Errno::EINVAL,
+          Errno::ECONNRESET,
+          EOFError,
+          Net::HTTPBadResponse,
+          Net::HTTPHeaderSyntaxError,
+          Net::ProtocolError,
+          Net::OpenTimeout,
+          Net::HTTPServerException,
+          Net::HTTPFatalError,
+          Mechanize::ResponseCodeError,
+          OpenSSL::SSL::SSLError,
+          Errno::EHOSTUNREACH,
+          Mechanize::Error,
+          Net::HTTP::Persistent::Error,
+          Net::HTTPRetriableError => e
+            Admin::BrokenLink.create(page_id: link.uri.to_s.split('/')[-1], link_text: l.text, page_title: link.text, broken_url: l.uri, link_status: 443, link_error: e)
           rescue SocketError => e
             Admin::BrokenLink.create(page_id: link.uri.to_s.split('/')[-1], link_text: l.text, page_title: link.text, broken_url: l.uri, link_status: 443, link_error: e)
           rescue OpenSSL::SSL::SSLError => e
