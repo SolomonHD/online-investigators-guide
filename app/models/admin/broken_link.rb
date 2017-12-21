7d710a504688d30bda98b7027f91fd3bb3fd2cdf
Saundra Barrett <sbarre5@emory.edu>
2017-12-21 16:00:16 -0500

    Waiting to hear back about server settings

--- app/models/admin/broken_link.rb
+++ app/models/admin/broken_link.rb
@@ -27,7 +27,6 @@ class Admin::BrokenLink < ApplicationRecord
         internalPage.links.each do |l|
           begin
            l.click.code.to_s
-           Admin::BrokenLink.create(page_id: link.uri.to_s.split('/')[-1], link_text: l.text, page_title: link.text, broken_url: l.uri, link_status: l.click.code.to_s, link_error: l.click.code.to_s)
           rescue Mechanize::ResponseCodeError => e
             case e.response_code
               when "404"
