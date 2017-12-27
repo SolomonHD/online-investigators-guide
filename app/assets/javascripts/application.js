e402f1785fd4502aabfca9324360f77d5ca40954
Saundra Barrett <sbarre5@emory.edu>
2017-12-27 11:04:04 -0500

    Added javascript to application file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,22 @@
 //= require trix
 //= require turbolinks
 //= require_tree .
+
+
+function searchUsers() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("userSearch");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("userTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
