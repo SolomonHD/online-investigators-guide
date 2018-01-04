e34e7cbaa4d25a6813dff47a9c6e7ac0d64e5dd5
Saundra Barrett <sbarre5@emory.edu>
2018-01-04 11:03:49 -0500

    Modified search script to include name and netID

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,9 +24,11 @@ function searchUsers() {
   table = document.getElementById("userTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
+
     td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+    td_1 = tr[i].getElementsByTagName("td")[1]
+    if (td || td_1) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 || td_1.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
