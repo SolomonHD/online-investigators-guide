9d09fa07b2e316c94d544d0eedbed543e8e3fffe
Saundra Barrett <sbarre5@emory.edu>
2018-01-10 11:54:19 -0500

    Fixed inability to delete

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,7 +1,7 @@
 class Label < ApplicationRecord
-  has_and_belongs_to_many :pages, dependent: :destroy
-  has_many :answers_labels, :class_name => 'AnswersLabel'
-  has_many :answers, dependent: :destroy, through: :answers_labels
+  # has_and_belongs_to_many :pages, dependent: :destroy
+  has_many :answers_label, :class_name => 'AnswersLabel'
+  has_many :answers, through: :answers_label
 
   validates_uniqueness_of :name, :message => '%{value} has already been taken'
 end
