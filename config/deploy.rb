11d2baa95f6154cf2bd98b3af9f656b29d89a5e0
Saundra Barrett <sbarre5@emory.edu>
2018-01-19 10:45:47 -0500

    changed deploy to accept enterprise

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.10.0"
 #set :pty, true
 set :application, "online_investigators_guide"
-set :repo_url, "git@github.com:emory-lits-uit/online-investigators-guide.git"
+set :repo_url, "git@github.service.emory.edu:LITS/online-investigators-guide.git"
 set :user, "deploy"
 set :stages, %w(development)
 set :passenger_restart_with_touch, true
