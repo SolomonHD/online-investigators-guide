75cb1b796bfd358029d725c9381320af8eec29f0
WH Demo <whdemo@emory.edu>
2018-02-19 20:58:04 -0800

    Change production deploy path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ role :web, %w{deploy@localhost}
 role :db, %w{deploy@localhost}, :primary => true
 set :branch, "develop"
 set :rails_env, "production"
-set :deploy_to, "/home/deploy/server_code"
+set :deploy_to, "/home/deploy/ServerCode"
 
